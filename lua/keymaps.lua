524b63cb2016ec4c4ae9d6b9433a7b05d3830de5
peetwerapat <peetwerapat.dev@gmail.com>
2025-07-08 19:42:54 +0700

    Add Keymap exit terminal mode

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -10,17 +10,17 @@ vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", opts)
 vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", opts)
 
 vim.keymap.set("n", "<leader>w", function()
-  vim.cmd("w")
-  vim.lsp.buf.format({ async = true })
+	vim.cmd("w")
+	vim.lsp.buf.format({ async = true })
 end, { desc = "Save + Format", noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>", { desc = "FZF Files" })
 
 vim.keymap.set("n", "<leader>ff", require("fzf-lua").files, {
-  desc = "Find Files (fzf)",
-  noremap = true,
-  silent = true,
+	desc = "Find Files (fzf)",
+	noremap = true,
+	silent = true,
 })
 
 vim.keymap.set("n", "<leader>tt", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal" })
-
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
