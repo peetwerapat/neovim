b180d0a36e282707b34d3d09ebdd13fa1ae2cf1f
peetwerapat <peetwerapat.dev@gmail.com>
2025-07-08 22:13:54 +0700

    Edit Keymap Format and Write

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,8 +8,8 @@ vim.keymap.set("i", "<C-k>", "<Up>", opts)
 vim.keymap.set("i", "<C-l>", "<Right>", opts)
 
 vim.keymap.set("n", "<leader>w", function()
-	vim.cmd("w")
-	vim.lsp.buf.format({ async = true })
+	vim.lsp.buf.format({ async = false })
+	vim.cmd("write")
 end, { desc = "Save + Format", noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>", { desc = "FZF Files" })
